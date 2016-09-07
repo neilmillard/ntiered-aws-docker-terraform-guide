4adc822fa9e16036e9b5b303f22f3d65e069cb21
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-09-07 11:59:51 +0100

    add comment for keypair

--- terraform/04.key-pairs.tf
+++ terraform/04.key-pairs.tf
@@ -1,3 +1,6 @@
+/* the command to generate a new keypair
+ssh-keygen -t rsa -C "insecure-deployer" -P '' -f ssh/insecure-deployer
+*/
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-airpair-example" 
   public_key = "${file(\"ssh/insecure-deployer.pub\")}"
